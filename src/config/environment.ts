4b90f3c634b36d397d0470480f948f656ab15022
manavshrivastava <manavshrivastava@hotmail.com>
2021-09-07 02:19:48 +0530

    added .env

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -4,15 +4,15 @@ import debug from 'debug';
 const log: debug.IDebugger = debug('config:environment');
 
 const load = () => {
-  if (dotenv.config().error) {
-    const dotenvResult = dotenv.config({ path: '.env.copy' });
-    if (dotenvResult.error) {
-      log('Error', 'Environment variables are not loaded !');
-      throw dotenvResult.error;
-    }
-  }
-  log('Environment variables are loaded');
-  return true;
+  // if (dotenv.config().error) {
+  //   const dotenvResult = dotenv.config({ path: '.env.copy' });
+  //   if (dotenvResult.error) {
+  //     log('Error', 'Environment variables are not loaded !');
+  //     throw dotenvResult.error;
+  //   }
+  // }
+  // log('Environment variables are loaded');
+  // return true;
 };
 
 export default load();
